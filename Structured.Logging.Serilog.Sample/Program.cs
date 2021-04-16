dfbad1286f9e181fde831110acf8b4384ebc63f1
Rajib Laha <rajib.laha@arcadis.com>
2021-04-16 11:25:26 +0530

    Added default project

---
+++ Structured.Logging.Serilog.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Structured.Logging.Serilog.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
